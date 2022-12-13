5ca532dca2b0fcd2d73cf2561b58942b4cc450e3
Nico1220 <nico01@aon.at>
2022-12-13 18:37:38 +0100

    CC

--- src/main/java/org/haupt/chemicals/api/model/Order.java
+++ src/main/java/org/haupt/chemicals/api/model/Order.java
@@ -4,7 +4,6 @@ import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.util.List;
 
 @Entity
--- src/main/java/org/haupt/chemicals/api/model/User.java
+++ src/main/java/org/haupt/chemicals/api/model/User.java
@@ -42,49 +42,5 @@ public class User {
     @Column(name = "u_role", nullable = true, length = 20)
     private String role;
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getRole(){return role;}
-//
-//    public void setRole(String role){this.role = role;}
-
 }
 
